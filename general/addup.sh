43b1387a161eda45c94ca7ef0b69656b761d939f
Jonathan Lovelace <kingjon3377@gmail.com>
2015-12-10 14:59:34 -0500

    addup.sh: Remove trailing whitespace.

--- general/addup.sh
+++ general/addup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 called_path=$_
 addup() {
-	expr $(sed -e :a -e '$!N;s/\n/ + /;ta') 
+	expr $(sed -e :a -e '$!N;s/\n/ + /;ta')
 }
 # Testing $_ (saved at the top of the script) against $0 isn't as reliable as
 # $BASH_SOURCE, but is portable to other sh implementations
