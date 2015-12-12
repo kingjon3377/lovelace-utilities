4fd51dad7eb5e628a98816393e6e52dfe6d9d7a8
Jonathan Lovelace <kingjon3377@gmail.com>
2015-12-11 23:05:51 -0500

    Remove obsolete man wrapper and reimplementation.

--- general/man.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh -v
-man "$1"
-rm -i "$(man -W "$1" | grep -v man/)"
--- general/man2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-for xx in "$@"
-do
-	(echo ".pl 11i" ; /bin/gunzip -c "$(man -w "$xx")") | 
-	/usr/bin/gtbl | 
-	/usr/bin/groff -Tlatin1 -mandoc | 
-	/usr/bin/less -is
-done
