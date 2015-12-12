0d7ade2a3d146156ea288bcb61b0d0feb58c8487
Jonathan Lovelace <kingjon3377@gmail.com>
2015-12-11 22:58:20 -0500

    Remove no-longer-used find_ext.sh and find_pdfs.sh: Use locate instead.

--- general/find_pdfs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd ~ || exit 1
-find . -path ./.mozilla -prune -o -path ./.avfs -prune -o -path ./temp -prune -o \( -iname \*.pdf -o -iname \*.pdf.gz -o -iname \*.pdf.bz2 -o -iname \*.pdf.rz -o -iname \*.pdf.lrz \) -print | grep -v ocr_later | grep -v keep_as_PDF | less
