4e65808026e4470a1e91938d1c595c0c52dace40
Jonathan Lovelace <kingjon3377@gmail.com>
2015-12-11 22:55:26 -0500

    find_ext.sh: Allow user to specify searched dirs
    Other than $HOME

--- general/find_ext.sh
+++ general/find_ext.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 cd ~ || exit 1
-find . /debian \( -path ./.mozilla -o -path ./.avfs -o -path ./temp \) -prune -o \( -iname "*.${1}" -o -iname "*.${1}.gz" -o -iname "*.${1}.bz2" -o -iname "*.${1}.rz" -o -iname "*.${1}.lrz" -o -iname "*.${1}.xz" \) -print
+if ! declare -p FIND_EXT_BASE > /dev/null; then
+    FIND_EXT_BASE=( /debian )
+fi
+find . "${FIND_EXT_BASE[@]}" \( -path ./.mozilla -o -path ./.avfs -o -path ./temp \) -prune -o \( -iname "*.${1}" -o -iname "*.${1}.gz" -o -iname "*.${1}.bz2" -o -iname "*.${1}.rz" -o -iname "*.${1}.lrz" -o -iname "*.${1}.xz" \) -print
