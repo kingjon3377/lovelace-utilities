ca9f2c83533996dc4bc3c220d9c370c1e0449f78
Jonathan Lovelace <kingjon3377@gmail.com>
2016-01-03 18:30:16 -0500

    suspend-after-emerge.sh: Make a function; add suppressions.

--- general/suspend-after-emerge.sh
+++ general/suspend-after-emerge.sh
@@ -1,16 +1,27 @@
 #!/bin/sh
+called_path=$_
 # FIXME: Support alternate suspend command, loading it from config
 # We want to use substitution on the parameters array, which I think is a bashism
-case $# in
-    0) pid=$(pidof -x emerge)
-        while test -d /proc/${pid:-invalid};do
-            sleep 720
-        done && sudo pm-suspend ;;
+suspend_after_emerge() {
+    case $# in
+        0)
+            # We want each PID to be checked individually
+            # shellcheck disable=SC2046
+            suspend_after_emerge $(pidof -x emerge) ;;
     1) while test -d "/proc/${1}";do
         sleep 720
     done && sudo pm-suspend ;;
-*) firstpid=${1};shift
-    while test -d "/proc/${firstpid}" ${@/#/-o /proc/};do
-        sleep 720
-    done && sudo pm-suspend ;;
-esac
+    *) firstpid=${1};shift
+        # We want this to be "double split" so the -o becomes a separate argument
+        # shellcheck disable=SC2068
+        while test -d "/proc/${firstpid}" ${@/#/-o /proc/};do
+            sleep 720
+        done && sudo pm-suspend ;;
+    esac
+}
+# Testing $_ (saved at the top of the script) against $0 isn't as reliable as
+# $BASH_SOURCE, but is portable to other sh implementations
+if [ "${called_path}" = "$0" ]; then
+#if [ "${BASH_SOURCE}" = "$0" ]; then
+	suspend_after_emerge "$@"
+fi
