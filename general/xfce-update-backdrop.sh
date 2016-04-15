fdac5763a35c43187b89c79ec0833320797658d6
Jonathan Lovelace <kingjon3377@gmail.com>
2016-04-15 02:02:01 -0400

    xfce-update-backdrop.sh: Use find (with a GNU-ism) rather than ls to get most recent DBUS session bus address.

--- general/xfce-update-backdrop.sh
+++ general/xfce-update-backdrop.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 if pidof xfdesktop > /dev/null ; then
 	if [ -z "${DBUS_SESSION_BUS_ADDRESS}" ] ; then
-        	newest_script="${HOME}/.dbus/session-bus/$(ls -rt "${HOME}/.dbus/session-bus/" | tail -1)"
+		newest_script="$(find "${HOME}/.dbus/session-bus" \
+								-type f -printf '%T@ %p\n' | 
+							sort -n | tail -n 1 | cut -f2- -d' ')"
         # shellcheck source=/dev/null
 		. "${newest_script}"
-        	export DBUS_SESSION_BUS_ADDRESS
+        export DBUS_SESSION_BUS_ADDRESS
 	fi
 	MONITOR=${1:-0}
 	PROPERTY="/backdrop/screen0/monitor${MONITOR}/image-path"
