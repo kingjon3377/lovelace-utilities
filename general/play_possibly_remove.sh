05b6236c45430f8a453af7e14a2b79174e326c2d
Jonathan Lovelace <kingjon3377@gmail.com>
2016-04-17 00:33:16 -0400

    play_possibly_remove.sh: Specify bash and use more reliable sourcing-detection mechanism, after something in my config broke the other.

--- general/play_possibly_remove.sh
+++ general/play_possibly_remove.sh
@@ -1,8 +1,9 @@
-#!/bin/sh
-cm_called_path=$_
-lib_path="${lib_path:-${cm_called_path:-${HOME}/bin/discarded}}"
+#!/bin/bash
+#cm_called_path=$_
+cm_called_path="${BASH_SOURCE[0]}"
+cm_lib_path="${cm_lib_path:-${cm_called_path:-${HOME}/bin/discarded}}"
 # shellcheck source=./lovelace-utilities-source-config.sh
-. "${lib_path%/*}/lovelace-utilities-source-config.sh" || return 1
+. "${cm_lib_path%/*}/lovelace-utilities-source-config.sh" || return 1
 play_possibly_remove() {
     lovelace_utilities_source_config
 	for a in "$@"; do
@@ -12,7 +13,7 @@ play_possibly_remove() {
 
 # Testing $_ (saved at the top of the script) against $0 isn't as reliable as
 # $BASH_SOURCE, but is portable to other sh implementations
-if [ "${cm_called_path}" = "$0" ]; then
-#if [ "${BASH_SOURCE[0]}" = "$0" ]; then
+#if [ "${cm_called_path}" = "$0" ]; then
+if [ "${BASH_SOURCE[0]}" = "$0" ]; then
         play_possibly_remove "$@"
 fi
