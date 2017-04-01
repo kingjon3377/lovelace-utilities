9ac208461e9e01e686e0dfa5b8d952a01f2cd35a
Jonathan Lovelace <kingjon3377@gmail.com>
2017-04-01 12:25:11 -0400

    upgrade_system.sh: By default dissable autounmask entirely, since we've seen it break resolution entirely too often lately.

--- general/upgrade_system.sh
+++ general/upgrade_system.sh
@@ -4,7 +4,8 @@ then
 	sudo "$0"
 else
 # --verbose-conflicts
-	emerge -auqvND --complete-graph --autounmask-keep-masks @system @selected @world && \
+	emerge -auqvND --complete-graph --autounmask=n @system @selected @world && \
+#	emerge -auqvND --complete-graph --autounmask-keep-masks @system @selected @world && \
 #	emerge -auqvND --complete-graph --backtrack=0 --autounmask-keep-masks @system @selected @world && \
 #	emerge -auqvND --complete-graph --backtrack=4 --autounmask-keep-masks @system @selected @world && \
 	emerge -a @preserved-rebuild --autounmask-keep-masks && \
