2ec76c819af218d97fe53a92c3b757d13b6b3dde
Jonathan Lovelace <kingjon3377@gmail.com>
2019-03-06 12:25:05 -0500

    upgrade_system.sh: Remove a bunch of options from 'emerge' calls, which I now store in EMERGE_DEFAULT_OPTS in /etc/portage/make.conf instead.

--- general/upgrade_system.sh
+++ general/upgrade_system.sh
@@ -3,13 +3,10 @@ if test "$(id -ru)" -ne 0
 then
 	sudo "$0"
 else
-# --verbose-conflicts
-	emerge -auqvND --complete-graph --autounmask=n @system @selected @world && \
-#	emerge -auqvND --complete-graph --autounmask-keep-masks @system @selected @world && \
-#	emerge -auqvND --complete-graph --backtrack=0 --autounmask-keep-masks @system @selected @world && \
-#	emerge -auqvND --complete-graph --backtrack=4 --autounmask-keep-masks @system @selected @world && \
+# Set options like --verbose-conflicts, --complete-graph, --autounmask=n,
+# --backtrack, --jobs, --load-average, etc. in EMERGE_DEFAULT_OPTS
+	emerge -auqvND @system @selected @world && \
 	emerge -a @preserved-rebuild --autounmask-keep-masks && \
-#	emerge -auvND @system @selected @world --jobs=2 --load-average=4 && \
 	emerge -a --depclean && \
 	revdep-rebuild -p -i
 fi
