dc297d0f6e73f6731315d751b390c0eb55ae32d1
Jonathan Lovelace <kingjon3377@gmail.com>
2019-07-24 20:00:35 -0400

    Convert whymask.sh to specify Bash, since it apparently uses a Bashism.

--- third_party/whymask.sh
+++ third_party/whymask.sh
@@ -1,14 +1,12 @@
-#!/bin/sh
-called_path=$_
+#!/bin/bash
 whymask() {
     find /usr/portage/profiles/ -name '*.mask' -exec \
         awk -vRS= "/${*/\//.}/ {
                 print \" \" FILENAME \":\", \"\n\" \"\n\" \$0 \"\n\"
         }" {} + | less
 }
-# Testing $_ (saved at the top of the script) against $0 isn't as reliable as
-# $BASH_SOURCE, but is portable to other sh implementations
-if [ "${called_path}" = "$0" ]; then
-#if [ "${BASH_SOURCE[0]}" = "$0" ]; then
+# We use more reliable sourcing-detection because string replacement is a
+# bashism.
+if [ "${BASH_SOURCE[0]}" = "$0" ]; then
         whymask "$@"
 fi
