dd5d45b03a4183a8b2820de6b777f04179ad0b54
Jonathan Lovelace <kingjon3377@gmail.com>
2019-08-17 17:08:03 -0400

    third_party/btfhindex.sh: Handle more variations seen in BtfH story Zip files.

--- third_party/btfhindex.sh
+++ third_party/btfhindex.sh
@@ -4,11 +4,64 @@
 simples=("" One Two Three Four Five Six Seven Eight Nine Ten Eleven Twelve Thirteen Fourteen Fifteen Sixteen Seventeen Eighteen Nineteen)
 aughts=("" -one -two -three -four -five -six -seven -eight -nine)
 tens=("" "" Twenty Thirty Forty Fifty Sixty Seventy Eighty Ninety)
-pres=(Introduction Intro Prologue Prolog)
+pres=(Foreword Introduction Intro Prologue Prolog)
 posts=(Epilogue Epilog)
 started=0
 
 # Functions
+to_roman() {
+	input=$1
+	output=""
+	len=${#input}   # Initial length to count down
+
+	roman_val() {
+		N=$1
+		one=$2
+		five=$3
+		ten=$4
+		out=""
+
+		case $N in
+		0)      out+="" ;;
+		[123])  while [[ $N -gt 0 ]]; do
+				out+="$one"
+				N=$((N-1))
+			done ;;
+		4)      out+="$one$five"        ;;
+		5)      out+="$five"    ;;
+		[678])  out+="$five"
+			N=$((N-5))
+			while [[ $N -gt 0 ]]; do
+				out+="$one"
+				N=$((N-1))
+			done ;;
+		9)      while [[ $N -lt 10 ]]; do
+				out+="$one"
+				N=$((N+1))
+			done
+			out+="$ten" ;;
+		esac
+		echo "$out"
+	}
+
+	while [[ $len -gt 0  ]]; do
+		num=${input:0:1}
+		case $len in
+		1) output+="$(roman_val "$num" I V X)" ;;
+		2) output+="$(roman_val "$num" X L C)" ;;
+		3) output+="$(roman_val "$num" C D M)" ;;
+		*) # 10'000 gets a line above, 100'000 gets a line on the left.. how to?
+			num=${input:0:(-3)}
+			while [[ $num -gt 0 ]]; do
+				output+="M"
+				num=$((num-1))
+			done ;;
+		esac
+		input=${input:1} ; len=${#input}
+	done
+	echo $output
+}
+
 chapter_found() {
 	if (( ! started )); then
 		echo "<html>"
@@ -72,8 +125,12 @@ generate_html() {
 			((ten=chapter_num / 10 % 10))
 			number=${tens[$ten]}${aughts[${aught}]}
 		fi
+		handle_chapter Part_${chapter_num}
+		handle_chapter "Part_${number}"
+		handle_chapter "Part_$(to_roman ${chapter_num})"
 		handle_chapter Chapter_${chapter_num}
 		handle_chapter "Chapter_${number}"
+		handle_chapter "Chapter_$(to_roman ${chapter_num})"
 	done
 	handle_chapters posts ${#posts[@]}
 
