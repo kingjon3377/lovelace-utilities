3df65382bf24abf1cfbda103e64c37c5eb92da60
Jonathan Lovelace <kingjon3377@gmail.com>
2022-03-14 07:43:11 -0400

    Add docgrep.sh, like epubgrep but for Microsoft Word files.

---
+++ general/docgrep.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+options=( )
+args=( )
+for arg in "$@"; do
+	if test "${double_dash:-n}" = y;then
+		args+=( "${arg}" )
+	else
+		case "${arg}" in
+		--) double_dash=y ;;
+		-*) options+=( "${arg}" ) ;;
+		*) args+=( "${arg}" ) ;;
+		esac
+	fi
+done
+for file in "${args[@]}"; do
+	case "${file}" in
+	*.doc) catdoc "$file" | grep -q "${options[@]}" && echo "$file" ;;
+	*.docx) docx2text "$file" | grep -q "${options[@]}" && echo "$file" ;;
+	*) grep -l "${options[@]}" -- "$file" ;;
+	esac
+done
