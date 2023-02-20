bd3587b9a78488d09eeff2874d615c090b158a6a
Jonathan Lovelace <kingjon3377@gmail.com>
2023-02-20 10:25:31 -0500

    Fix find_largest_packages.sh
    qsize doesn't have a -a option (anymore, if it ever did), and its output with -k now
    says KiB instead of KB.

--- general/find_largest_packages.sh
+++ general/find_largest_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 find_largest_packages() {
-	qsize -a -k -f -C | sed -e 's/^\([^:]*\): [0-9]* files, [0-9]* non-files, \([0-9]*\) KB$/\2	\1/' | sort -nr | less
+	qsize -k -f -C | sed -e 's/^\([^:]*\): [0-9]* files, [0-9]* non-files, \([0-9]*\) KiB$/\2	\1/' | sort -nr | less
 }
 if [ "${BASH_SOURCE[0]}" = "$0" ]; then
 	find_largest_packages "$@"
